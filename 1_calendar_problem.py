dd2ff838c1994ec47bc90d8b545030f40e524697
nijes <monochrome05@gmail.com>
2023-01-04 15:31:24 +0900

    'assignment_1'

--- 1_calendar_problem.py
+++ 1_calendar_problem.py
@@ -5,13 +5,14 @@ def sunday_1st():
         for month in range(1, 13):
             # Check if the first day of the month is Sunday
             if day_diff % 7 == 6:
+                print(year, month)
                 answer += 1
             # Add the number of days in the month to 'day_diff'
             if month in [1, 3, 5, 7, 8, 10, 12]:
                 day_diff += 31
             elif month in [4, 6, 9, 11]:
                 day_diff += 30
-            elif year % 4 == 0 and year % 100 != 0:
+            elif year % 4 == 0 and year % 400 not in [100, 200, 300]:
                 day_diff += 29
             else:
                 day_diff += 28
